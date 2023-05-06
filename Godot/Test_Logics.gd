2d6edc20fd52bb01fa789c5546011a10a2eabe8d
AdilarMaia <adilar_maia@yahoo.com.br>
2023-05-06 00:21:47 -0300

    Update Test_Logics.gd

--- Godot/Test_Logics.gd
+++ Godot/Test_Logics.gd
@@ -3,8 +3,14 @@ extends Node
 var nome = "Adilar" # string
 var idade = 43      # inteiro
 var altura = 1.60   # float
+var altura_media = 1.80
 
 func _ready():
     print("Meu nome é ", nome)
     print("Minha idade é ", idade)
     print("Minha altura é ", altura)
+
+    if altura_media > altura:
+        print("Você é baixo...")
+    else:
+        print("Você é alto...")
