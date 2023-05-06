98d3c5beb802075169a07a2a4be2d785d0b35fdd
AdilarMaia <adilar_maia@yahoo.com.br>
2023-05-06 00:18:48 -0300

    Update Test_Logics.gd

--- Godot/Test_Logics.gd
+++ Godot/Test_Logics.gd
@@ -1,6 +1,10 @@
 extends Node
 
-var nome = "Adilar"
+var nome = "Adilar" # string
+var idade = 43      # inteiro
+var altura = 1.60   # float
 
 func _ready():
     print("Meu nome é ", nome)
+    print("Minha idade é ", idade)
+    print("Minha altura é ", altura)
